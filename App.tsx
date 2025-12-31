e2195dbebf90a47671741d08e2638cd29b6638df
caesar-dat-com <54614170+caesar-dat-com@users.noreply.github.com>
2025-12-31 08:01:18 -0500

    Use main tab navigator in App

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import GlassDemoScreen from './src/screens/GlassDemoScreen';
+import TabNavigator from './src/navigation/TabNavigator';
 
 function App(): JSX.Element {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
         <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-        <GlassDemoScreen />
+        <TabNavigator />
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
